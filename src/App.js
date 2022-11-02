c792f945c2020200ff7650dbe85ab75904a65e2c
youngeun choi <choi46910@naver.com>
2022-11-02 15:16:50 +0900

    온보딩 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import TopBanner from "@components/templates/TopBanner";
 import Events from "@components/templates/Events";
 import ItemsOnSale from "@components/templates/ItemsOnSale";
 import OpenseaTopCollection from "@components/templates/OpenseaTopCollection";
-
-const OnBoarding = styled.div`
-  width: 100%;
-  height: 415px; 
-  background-color: teal;
-`;
+import OnBoarding from "@components/templates/OnBoarding";
 
 const Footer = styled.footer`
   height: 240px;
